c730473604539f0dbe983074098c885e2992170e
Hikari Sakamoto <hikari.sakamoto@dbserver.com.br>
2019-10-09 20:17:17 -0300

    Implementation of notifications at shared kernel

---
+++ src/Domain.Core/Sakamoto.TCC2.CSU.Domain.Core/Sakamoto.TCC2.CSU.Domain.Core/Notifications/DomainNotificationHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Sakamoto.TCC2.CSU.Domain.Core.Notifications
+{
+    public class DomainNotificationHandler : INotificationHandler<DomainNotification>, IDisposable
+    {
+        private ICollection<DomainNotification> _notifications;
+
+        public Task Handle(DomainNotification notification, CancellationToken cancellationToken)
+        {
+            _notifications.Add(notification);
+
+            return Task.CompletedTask;
+        }
+
+        public virtual ICollection<DomainNotification> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public virtual bool HasNotifications()
+        {
+            return GetNotifications().Any();
+        }
+
+        public void Dispose()
+        {
+            _notifications = new List<DomainNotification>();
+        }
+    }
+}
