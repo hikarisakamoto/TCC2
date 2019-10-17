f90904b82fcc0c204bd1692903b624a7b4212564
Hikari Sakamoto <hikari.sakamoto@dbserver.com.br>
2019-10-16 21:32:12 -0300

    Added base context for patient

---
+++ src/Paciente/Sakamoto.TCC2.CSU.Paciente.Domain/EventHandlers/PatientEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Sakamoto.TCC2.CSU.Patients.Domain.Events;
+
+namespace Sakamoto.TCC2.CSU.Patients.Domain.EventHandlers
+{
+    public class PatientEventHandler : INotificationHandler<PatientRegisteredEvent>, INotificationHandler<PatientDeactivatedEvent>, INotificationHandler<PatientUpdatedEvent>
+    {
+        public Task Handle(PatientRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO SEND PATIENT REGISTERED EVENT MESSAGE
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(PatientDeactivatedEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO SEND PATIENT DEACTIVATED EVENT MESSAGE
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(PatientUpdatedEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO SEND PATIENT UPDATED EVENT MESSAGE
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Paciente/Sakamoto.TCC2.CSU.Patients.Infrastructure.Data/Context/PatientContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sakamoto.TCC2.CSU.Patients.Infrastructure.Data.Context
+{
+    public class PatientContext : DbContext
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        protected PatientContext(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+    }
+}
